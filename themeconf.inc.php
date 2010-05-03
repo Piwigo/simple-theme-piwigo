b1af7822fe3efd49ab97002b9aa081e3ff50a749
Simon <contact/saimon.org>
2010-05-03 22:16:55 +0200

    update themeconf.inc.php + move theme.css in the root directory

---
+++ themeconf.inc.php
@@ -0,0 +1,23 @@
+<?
+/*
+Theme Name: Simple Grey
+Version: 2.0
+Description: Simple Grey theme
+Theme URI: http://saimon.org/blog/pages/Theme-Simple-Grey-Piwigo
+Author: Saïmon
+Author URI: http://saimon.org/
+*/
+
+$themeconf = array(
+  'parent' => 'default',
+  'name' => 'simple-grey',
+  'icon_dir' => 'themes/simple/icon',
+  'admin_icon_dir' => 'themes/default/icon/admin',
+  'mime_icon_dir' => 'themes/default/icon/mimetypes/',
+  'local_head' => '<!-- no theme specific head content -->'
+  'url' => 'http://saimon.org/blog/pages/Theme-Simple-Grey-Piwigo',
+);
+
+global $user;
+$user['maxwidth'] = 680;
+?>
