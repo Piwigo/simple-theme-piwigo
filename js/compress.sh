9a7b84b8e13f811671d1c2db76225f2bdecde362
Simon <contact@saimon.org>
2011-04-04 23:59:26 +0200

    cleanup useless javascript stuff

--- js/compress.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# combine and minify scripts
-
-rm scripts.js scripts.min.js
-cat simplescripts.js pwgscripts.js rating.js jquery.cookie.min.js > scripts.js
-yuicompressor --nomunge --type js scripts.js > scripts.min.js
