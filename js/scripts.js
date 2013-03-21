e784dfe9d6758f2e38d36a2b8266f83121a4bda8
Simon <contact@saimon.org>
2013-03-22 00:52:03 +0100

    Update infinitescroll and try to make it work (but this is a fail !)

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,13 @@ jQuery(document).ready(function($){
   jQuery('.navbar .brand').html(jQuery('#theHeader h1').html());
   jQuery('#theHeader').hide();
 
-  $('#thumbnails ul').infinitescroll({
+  $('ul.thumbnails').infinitescroll({
+    // debug        : true,
     navSelector  : "div.navigationBar",
     // selector for the paged navigation (it will be hidden)
     nextSelector : "div.navigationBar a.next",
     // selector for the NEXT link (to page 2)
-    itemSelector : "#thumbnails li",
+    itemSelector : ".thumbnails li",
     // selector for all items you'll retrieve
     loadingText  : "Loading ...",
     donetext     : ""
